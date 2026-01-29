5d743ed4742cf2931e2c1b80d275cc2d3564cc64
aobuya <outlookpc1>
2026-01-29 19:54:01 +0300

    added fix for typos

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45" # It's good practice to lock the version
+    }
+  }
+}
+
+variable "hcloud_token" {
+  sensitive = true
+}
+
 provider "hcloud" {
   token = var.hcloud_token
 }
 
-variable "hcloud_token" { sensitive = true }
-
+# Your test resource
 resource "hcloud_network" "test_net" {
   name     = "automation-network"
   ip_range = "10.0.0.0/16"
